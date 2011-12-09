31b765f2d23bd2b90bd81620437ae3b5dd6b9db0
Ricardo Gomez <ricardo.gomez331@gmail.com>
2011-12-09 17:53:45 -0600

    Tracker will parse paramaters.

--- st.py
+++ st.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python
 from BaseHTTPServer import (HTTPServer, BaseHTTPRequestHandler)
-import urlparse
+from bt import bencode
+from urlparse import parse_qs
 
 class Tracker(BaseHTTPRequestHandler):
 
     server_version = 'PyTracker/0.0001'
 
     def do_GET(self):
-        print self.headers
+        start = self.path.index('?')+1
+        rargs = self.path[start:]
+        rargs = parse_qs(rargs, True)
+
+        response = {}
+        response['interval'] = 30000
+        if hasattr(rargs, 'tracker_id'):
+            response['tracker_id'] = self.tid
+        response['complete'] = 0
+        response['incomplete'] = 0
+        response['peers'] = []
+        for p in []:
+            response['peers'].append(p.compact)
+
+        response = bencode.bencode(response)
         self.send_response(200)
         self.end_headers()
-        self.wfile.write('TBD')
+        self.wfile.write(response)
         return
 
 if __name__ == '__main__':
